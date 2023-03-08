359a296d39eea47595c93fcf9089c278f19bec11
David <dfernandez.alumno@iesrafaelalberti.es>
2023-03-08 03:06:53 +0100

    initial commit

---
+++ src/main/java/com/travel/blog/TravelBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.travel.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBlogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelBlogApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/travel/blog/TravelBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
