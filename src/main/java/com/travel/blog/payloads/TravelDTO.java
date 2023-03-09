a6e2bae76b220f4c75930232cc1e3a1c4502b9a0
David <dfernandez.alumno@iesrafaelalberti.es>
2023-03-09 02:30:07 +0100

    add show comments list on travels response

--- src/main/java/com/travel/blog/payloads/TravelDTO.java
+++ src/main/java/com/travel/blog/payloads/TravelDTO.java
@@ -3,6 +3,7 @@ package com.travel.blog.payloads;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.Set;
 
 @Data
 public class TravelDTO {
@@ -12,4 +13,5 @@ public class TravelDTO {
     private String image;
     private Date date;
     private String location;
+    private Set<CommentDTO> comments;
 }
