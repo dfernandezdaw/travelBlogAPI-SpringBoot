7e091e665ff37983428e7e278aa3db0976b819a0
David <dfernandez.alumno@iesrafaelalberti.es>
2023-03-09 10:02:52 +0100

    add update category

--- src/main/java/com/travel/blog/controllers/CategoriesController.java
+++ src/main/java/com/travel/blog/controllers/CategoriesController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/")
 public class CategoriesController {
@@ -15,15 +17,31 @@ public class CategoriesController {
         this.categoriesService = categoriesService;
     }
 
+    //Create category
     @PostMapping("/categories")
     public ResponseEntity<CategoriesDTO> createCategories(@RequestBody CategoriesDTO categoriesDTO) {
         return new ResponseEntity<>(categoriesService.createCategories(categoriesDTO), HttpStatus.CREATED);
     }
 
+    //Get category by id
     @GetMapping("/categories/{categoriesId}")
     public ResponseEntity<CategoriesDTO> getCategoriesById(@PathVariable Long categoriesId) {
         CategoriesDTO categoriesDTO = categoriesService.getCategoriesById(categoriesId);
         return new ResponseEntity<>(categoriesDTO, HttpStatus.OK);
+    }
+
+    //Get all categories
+    @GetMapping("/categories")
+    public List<CategoriesDTO> getAllCategories() {
+        return categoriesService.getAllCategories();
+    }
+
+    //Update category by id
+    @PutMapping("/categories/{categoriesId}")
+    public ResponseEntity<CategoriesDTO> updateCategories(@PathVariable Long categoriesId,
+                                                          @RequestBody CategoriesDTO categoriesDTO) {
+        CategoriesDTO updatedCategories = categoriesService.updateCategories(categoriesId, categoriesDTO);
+        return new ResponseEntity<>(updatedCategories, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/travel/blog/services/impl/CategoriesServiceImpl.java
+++ src/main/java/com/travel/blog/services/impl/CategoriesServiceImpl.java
@@ -9,6 +9,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoriesServiceImpl implements CategoriesService {
@@ -36,12 +37,20 @@ public class CategoriesServiceImpl implements CategoriesService {
 
     @Override
     public List<CategoriesDTO> getAllCategories() {
-        return null;
+        List<Categories> categories = categoriesRepository.findAll();
+        return categories.stream().map(this::convertToDTO).collect(Collectors.toList());
     }
 
     @Override
     public CategoriesDTO updateCategories(Long categoriesId, CategoriesDTO categoriesDTO) {
-        return null;
+        Categories categories = categoriesRepository.findById(categoriesId).orElseThrow(
+                () -> new ResourceNotFoundException("Category", "id", categoriesId));
+
+        categories.setName(categoriesDTO.getName()); //Update name
+
+        Categories updatedCategories = categoriesRepository.save(categories);
+
+        return convertToDTO(updatedCategories);
     }
 
     @Override
